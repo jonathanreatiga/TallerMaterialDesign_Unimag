213ece15073835e5f9ee3ac5ecaaacc1ede187c1
jonathanreatiga <joalrezu2@gmail.com>
2020-06-07 14:37:43 -0500

    Archivos iniciales

---
+++ app/src/main/java/com/example/tallermaterialdesign/DetalleVehiculo.java
@@ -0,0 +1,14 @@
+package com.example.tallermaterialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalleVehiculo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_vehiculo);
+    }
+}
---
+++ app/src/main/java/com/example/tallermaterialdesign/Vehiculo.java
@@ -0,0 +1,4 @@
+package com.example.tallermaterialdesign;
+
+public class Vehiculo {
+}
